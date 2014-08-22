787aae6d7dabf6ed617aba17f114953f914dbe0f
Niklas Andersson <niklas.andersson@ericsson.com>
2014-08-22 21:48:10 +0200

    * Added script to time login

---
+++ time_ssh_login.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import pexpect
+import sys
+import time
+
+EXPECT_YESNO    = "\(yes/no\)\? "
+
+def connect(ipaddr,user,password):
+	print("Connecting to %s" % ipaddr)
+	child = pexpect.spawn('ssh %s@%s' % (user,ipaddr))
+	index = child.expect(['password: ',EXPECT_YESNO])
+	if index == 1:
+		print("Automatically accepting hostkey.")
+		child.sendline('yes')
+		child.expect('password')
+	child.sendline(password)
+	child.expect('~\$ ')
+	child.sendline('id')
+	child.expect('~\$ ')
+	child.sendline('exit')
+	child.expect('closed.')
+	
+def main():
+	start = time.time()
+	print("This is main")
+	connect(sys.argv[1],sys.argv[2],sys.argv[3])
+	end = time.time()
+	result = end - start
+	print("It took %s seconds" % result)
+
+if __name__ == '__main__':
+	main()
